8d18cc1193adaae5bc4d80947dcc8ef7e289602d
Aleš Aubrecht <alesaubr@gmail.com>
2025-02-23 16:35:46 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,70 @@
+<template>
+
+  <main>
+    <article class="desktop">
+      <section>
+        <img src="./assets/images/my-computer.ico" alt="My Computer logo">
+        <p>My Computer</p>
+      </section>
+      <section>
+        <img src="./assets/images/network-neighborhood.ico" alt="Network Neighborhood logo">
+        <p>Network Neighborhood</p>
+      </section>
+      <section>
+        <img src="./assets/images/inbox.ico" alt="Inbox logo">
+        <p>Inbox</p>
+      </section>
+      <section>
+        <img src="./assets/images/recycle-bin.ico" alt="Recycle Bin logo">
+        <p>Recycle Bin</p>
+      </section>
+      <section>
+        <img src="./assets/images/the-internet.ico" alt="The Internet logo">
+        <p>The Internet</p>
+      </section>
+    </article>
+    <article class="window">
+      <section class="title-bar">
+        <section>
+          <img src="./assets/images/my-computer.ico" alt="My Computer logo">
+          <p>My computer</p>
+        </section>
+        <button>
+          <span>X</span>
+        </button>
+      </section>
+      <section class="window-content">
+        <button>
+          <span>
+            Earn
+          </span>
+          <img src="./assets/images/windows-95.svg" alt="Earn logo">
+        </button>
+        <p>
+          +0/s
+        </p>
+        <section class="earn">
+          <p>
+            0
+          </p>
+          <img src="./assets/images/windows-95.svg" alt="Earn logo">
+        </section>
+      </section>
+    </article>
+  </main>
+
+  <footer>
+    <button>
+      <img src="./assets/images/windows-95-header.png" alt="Windows 95 logo">
+      <span>Start</span>
+    </button>
+  </footer>
+</template>
+
+<script lang="ts">
+
+</script>
+
+<style lang="scss">
+@use "./assets/styles/style.scss";
+</style>
