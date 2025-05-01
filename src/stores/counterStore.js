b2f91a723c604145f7ae71180d807be83113aa47
Aleš Aubrecht <alesaubr@gmail.com>
2025-05-01 17:20:17 +0200

    fix: add missing saving of per click value to local storage

--- src/stores/counterStore.js
+++ src/stores/counterStore.js
@@ -3,19 +3,24 @@ import {defineStore} from "pinia"
 import {useUpgradesStore} from "@/stores/upgradesStore.js";
 
 const KEY = "count";
+const PER_CLICK_KEY = "perClick";
 
 export const useCounterStore = defineStore("counterStore", () => {
     const upgradesStore = useUpgradesStore();
     const count = ref(Number(localStorage.getItem(KEY) ?? "0"))
-    const perClick = ref(1);
+    const perClick = ref(Number(localStorage.getItem(PER_CLICK_KEY) ?? "1"))
 
     function increment() {
         count.value += perClick.value;
     }
 
-   watch(() => count.value, () => {
-       localStorage.setItem(KEY, count.value);
-   })
+    watch(() => count.value, () => {
+        localStorage.setItem(KEY, count.value);
+    })
+
+    watch(perClick, () => {
+        localStorage.setItem(PER_CLICK_KEY, perClick.value);
+    });
 
     return {count, perClick, increment}
 })
