ee116a7001cc005b47e1fba0efb9da191dafbf1f
Aleš Aubrecht <alesaubr@gmail.com>
2025-05-03 00:31:34 +0200

    feat: add currency gain while offline

--- src/stores/counterStore.js
+++ src/stores/counterStore.js
@@ -1,12 +1,16 @@
-import {ref, watch} from "vue"
+import {ref, watch, onMounted} from "vue"
 import {defineStore} from "pinia"
+import {useUpgradesStore} from "@/stores/upgradesStore.js";
 
 const KEY = "count";
 const PER_CLICK_KEY = "perClick";
 const PER_SECOND_KEY = "perSecond";
 const REINSTALL_MULTIPLIER_KEY = "reinstallMultiplier";
+const DATE_KEY = "lastActive";
 
 export const useCounterStore = defineStore("counterStore", () => {
+    const upgradesStore = useUpgradesStore();
+
     const count = ref(Number(localStorage.getItem(KEY) ?? "0"))
     const perClick = ref(Number(localStorage.getItem(PER_CLICK_KEY) ?? "1"))
     const perSecond = ref(Number(localStorage.getItem(PER_SECOND_KEY) ?? "0"));
@@ -30,6 +34,7 @@ export const useCounterStore = defineStore("counterStore", () => {
 
     watch(() => count.value, () => {
         localStorage.setItem(KEY, count.value);
+        localStorage.setItem(DATE_KEY, new Date().toISOString());
     })
 
     watch(() => perClick.value, () => {
@@ -42,6 +47,25 @@ export const useCounterStore = defineStore("counterStore", () => {
 
     watch(() => reinstallMultiplier.value, () => {
         localStorage.setItem(REINSTALL_MULTIPLIER_KEY, reinstallMultiplier.value);
+    })
+
+    onMounted(() => {
+        const lastActive = localStorage.getItem(DATE_KEY);
+
+        if (!lastActive) return;
+
+        const lastActiveDate = new Date(lastActive);
+        const current = new Date();
+
+        const diff = (current - lastActiveDate) / 1000;
+
+        if (diff < 5) return;
+
+        const toAdd = Math.floor(diff * perSecond.value * reinstallMultiplier.value * 0.2);
+
+        count.value += toAdd;
+
+        upgradesStore.logs.push("System restored +" + toAdd + " recovered while offline");
     })
 
     return {count, perClick, perSecond, reinstallMultiplier, increment, reset}
